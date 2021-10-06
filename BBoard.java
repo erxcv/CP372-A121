6b144bf5f4b09dce62ff7715225a21e241f09715
grantw13 <78278076+grantw13@users.noreply.github.com>
2021-10-05 23:06:03 -0400

    Update BBoard.java

--- BBoard.java
+++ BBoard.java
@@ -1,10 +1,12 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
@@ -13,14 +15,16 @@ import java.util.ArrayList;
 public class BBoard {
     public static int width;
     public static int height;
-    public ArrayList<Post> posts = new ArrayList<Post>();
-    public ArrayList<Pin> pins = new ArrayList<Pin>();
-    String[] colors;
+    public static ArrayList<Post> posts = new ArrayList<Post>();
+    public static ArrayList<Pin> pins = new ArrayList<Pin>();
+    static String[] colors;
+    static String seperator = "$$";
              
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int port = Integer.parseInt(args[0]);
         height = Integer.parseInt(args[2]);
 	width = Integer.parseInt(args[1]);
+
         
         if (args.length > 3) {
             String[] colors = new String[args.length - 3];
@@ -31,11 +35,128 @@ public class BBoard {
                 i++;
             }
         }
-        
+        ServerSocket run = new ServerSocket(port);
+        try {
+            while (true) {
+                new server(run.accept()).start();
+            }    
+        } finally {
+            run.close();
+        }
     
     }
+    
+    public static class server extends Thread {
+        Socket socket;
+        
+        public server(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                BufferedReader input = new BufferedReader(new 
+                                InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(),
+                                                                        true);
+                
+                while(true) {
+                    String client = input.readLine();
+                    String[] temp = client.split(seperator);
+                    boolean result;
+                    
+                    switch(temp[0]) {
+                        case "UNPIN":
+                            result = unpin(Integer.parseInt(temp[1]), 
+                                                Integer.parseInt(temp[2]));
+                            if (result) {
+                                output.println("Pin (" + temp[1] + "," + temp[2] 
+                                            + ") was successfully removed.");
+                            } else {
+                                output.println("Specified pin does not exist.");
+                            }
+                            
+                        case "PIN":
+                            result = pin(Integer.parseInt(temp[1]), 
+                                                    Integer.parseInt(temp[2]));
+                            if (result) {
+                                output.println("Pin (" + temp[1] + "," + temp[2] 
+                                            + ") was successfully created.");
+                            } else {
+                                output.println("Specified location does not "
+                                        + "     exist or pin already exists.");
+                            }
+                            
+                        case "GET PINS":
+                            String message = "";
+                            for (Pin pin : pins) {
+                                message = message + pin.getPinstr() + "\n";
+                            } 
+                            
+                            output.println(message);
+                            
+                        case "SHAKE":
+                            shake();
+                            
+                        case "CLEAR":
+                            clear();
+                        case "POST":
+                            if (temp.length == 7){
+                                result = makePost(Integer.parseInt(temp[1]), 
+                           Integer.parseInt(temp[2]), Integer.parseInt(temp[3]), 
+                           Integer.parseInt(temp[4]), temp[5], temp[6]);
+                            } else {
+                                result = makePost(Integer.parseInt(temp[1]), 
+                           Integer.parseInt(temp[2]), Integer.parseInt(temp[3]), 
+                           Integer.parseInt(temp[4]), "", temp[5]);
+                            }
+                            
+                            if (result) {
+                                output.println("Post successfully created.");
+                            } else {
+                                output.println("Unable to create post. Please "
+                                        + "check the validity of your input.");
+                            }
+                        case "GET":
+                            String c, co, r;
+                            
+                            if ((temp[1].split("=")).length < 2) {
+                                c = "";
+                            } else {
+                                c = temp[1].split("=")[1];
+                            }
+                            if ((temp[2].split("=")).length < 2) {
+                                r = "";
+                            } else {
+                                r = temp[2].split("=")[1];
+                            }
+                            if ((temp[1].split("=")).length < 2) {
+                                co = "";
+                            } else {
+                                co = temp[1].split("=")[1];
+                            }
+                            
+                            String info = get(c,r,co);
+                            
+                            output.println(info);
+                            
+                    }
+                }
+                
+            } catch (IOException e) {
+		System.out.println(e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println(e);
+		}
+            }
+        
+        }
+}
 
-    public class Post {
+    public static class Post {
         int x, y, w, h, numPin;
         String color, content;
         boolean isPin;
@@ -93,7 +214,7 @@ public class BBoard {
         
     }
     
-    public class Pin {
+    public static class Pin {
         int x, y;
         
         public Pin(int x, int y) {
@@ -115,7 +236,7 @@ public class BBoard {
         }
     }
     
-    public boolean checkPost(int x, int y, int w, int h, String color) {
+    public static boolean checkPost(int x, int y, int w, int h, String color) {
         boolean check = false;
 
         if (colors!= null && colors.length > 0) {
@@ -128,11 +249,12 @@ public class BBoard {
         return (x + w < width && y + h < height && check);
     }
     
-    public boolean checkPin(int x, int y, int w, int h, int x1, int y1) {
+    public static boolean checkPin(int x, int y, int w, int h, int x1, int y1) {
         return (x <= x1 && x1 <= x + w && y <= y1 && y1 <= y + h);
     }
     
-    public boolean makePost(int x, int y, int w, int h, String content, String color) {
+    public static boolean makePost(int x, int y, int w, int h, String content, 
+                                                                String color) {
         if (checkPost(x,y,w,h,color)) {
             posts.add(new Post(x,y,w,h,content,color));
         } else {
@@ -141,7 +263,7 @@ public class BBoard {
         return true;
     }
     
-    public ArrayList<Post> refers(String s, ArrayList<Post> p) {
+    public static ArrayList<Post> refers(String s, ArrayList<Post> p) {
         ArrayList<Post> n = new ArrayList<Post>();
         for (Post post : p) {
             if (post.getContent().contains(s)) {
@@ -151,7 +273,7 @@ public class BBoard {
         return n;
     }
     
-    public ArrayList<Post> color(String c, ArrayList<Post> p) {
+    public static ArrayList<Post> color(String c, ArrayList<Post> p) {
         ArrayList<Post> n = new ArrayList<Post>();
         for (Post post : p) {
             if (post.getColor().equals(c)) {
@@ -161,7 +283,7 @@ public class BBoard {
         return n;
     }
     
-    public void shake() {
+    public static void shake() {
         ArrayList<Post> temp = new ArrayList<Post>();
         for (Post post : posts) {
             if (post.isPin()) {
@@ -171,39 +293,60 @@ public class BBoard {
         posts = temp;
     }
     
-    public void clear() {
+    public static void clear() {
         posts = new ArrayList<Post>();
         pins = new ArrayList<Pin>();
     }
     
-    public void unpin(int x, int y) {
+    public static boolean unpin(int x, int y) {
+        boolean check = true;
+        
         for (int i = 0; i < pins.size(); i++) {
             if (pins.get(i).getPinX() == x && pins.get(i).getPinY() == y) {
                 pins.remove(i);
+                check = false;
             }
         }
+        if (check) {
+            return false;
+        }
+        
         for (Post post : posts) {
-            if (checkPin(post.getX(), post.getY(), post.getWidth(), post.getHeight(), x, y)) {
+            if (checkPin(post.getX(), post.getY(), post.getWidth(),
+                                                    post.getHeight(), x, y)) {
                 if (post.isPin() && post.getnumPin() == 1) {
                     post.setPin(false);
                     post.updatenumPin(-1);
                 }
             }
         }
+        return true;
     }
     
-    public void pin(int x, int y) {
-        pins.add(new Pin(x,y));
+    public static boolean pin(int x, int y) {
+        
+        if (!checkPin(0,0, width, height, x, y)) {
+            return false;
+        }
+        Pin temp = new Pin(x,y);
+        for (Pin pin : pins) {
+            if (pin.getPinX() == x && pin.getPinY() == y) {
+                return false;
+            }
+        }
+        pins.add(temp);
         for (Post post : posts) {
-            if (checkPin(post.getX(), post.getY(), post.getWidth(), post.getHeight(), x, y)) {
+            if (checkPin(post.getX(), post.getY(), post.getWidth(), 
+                                                    post.getHeight(), x, y)) {
                 post.updatenumPin(1);
                 if (!post.isPin())
                     post.setPin(true);
             }
         }
+        return true;
     }
     
-    public String get(String color, String refers, String contains) {
+    public static String get(String color, String refers, String contains) {
         String out = "";
         ArrayList<Post> temp = new ArrayList<Post>();
         boolean check = false;
@@ -224,13 +367,15 @@ public class BBoard {
             String[] c = contains.split(" ", 2);
             if (!check) {
                 for (Post post : posts) {
-                    if (checkPin(post.getX(), post.getY(), post.getWidth(), post.getHeight(), Integer.parseInt(c[0]), Integer.parseInt(c[1]))) {
+                    if (checkPin(post.getX(), post.getY(), post.getWidth(), 
+            post.getHeight(), Integer.parseInt(c[0]), Integer.parseInt(c[1]))) {
                         temp.add(post);
                     }
                 }
             } else {
                 for (Post post : temp) {
-                    if (checkPin(post.getX(), post.getY(), post.getWidth(), post.getHeight(), Integer.parseInt(c[0]), Integer.parseInt(c[1]))) {
+                    if (checkPin(post.getX(), post.getY(), post.getWidth(), 
+           post.getHeight(), Integer.parseInt(c[0]), Integer.parseInt(c[1]))) {
                         temp.add(post);
                     }
                 }
@@ -244,8 +389,3 @@ public class BBoard {
         return out;
     }
 }
-
-
-
-
- 
